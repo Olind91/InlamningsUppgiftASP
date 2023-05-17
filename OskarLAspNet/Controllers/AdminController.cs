90d876c12d068ab0ced7be9f69ef84df2434327b
Olind91 <111879063+Olind91@users.noreply.github.com>
2023-05-17 10:16:53 +0200

    Add project files.

---
+++ OskarLAspNet/Controllers/AdminController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OskarLAspNet.Contexts;
+using OskarLAspNet.Helpers.Services;
+using OskarLAspNet.Models.Dtos;
+using OskarLAspNet.Models.Identity;
+
+namespace OskarLAspNet.Controllers
+{
+
+    public class AdminController : Controller
+    {
+        private readonly DataContext _dataContext;
+        private readonly ProductService _productService;
+        private readonly ContactFormService _contactFormService;
+
+        public AdminController(DataContext dataContext, ProductService productService, ContactFormService contactFormService)
+        {
+            _dataContext = dataContext;
+            _productService = productService;
+            _contactFormService = contactFormService;
+        }
+
+        [Authorize(Roles = "admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Authorize(Roles = "admin")]
+        public IActionResult GetAllUsers()
+        {
+            List<AppUser> userList = _dataContext.Users.ToList();
+            return View(userList);
+            
+        }
+
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> AdminProducts()
+        {
+
+            var products = await _productService.GetAllAsync();
+            return View(products);
+
+        }
+
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> ShowAllComments()
+        {
+
+            var comments = await _contactFormService.GetAllAsync();
+            return View(comments);
+
+        }
+
+    }
+}
---
+++ OskarLAspNet/Controllers/ProductsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OskarLAspNet.Helpers.Services;
+using OskarLAspNet.Models.ViewModels;
+
+namespace OskarLAspNet.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ProductService _productService;
+
+        public ProductsController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+
+
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> Create(ProductRegVM viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var product = await _productService.CreateProductAsync(viewModel);
+                if (product != null)
+                {
+                    if (viewModel.Image != null)
+                        await _productService.UploadImageAsync(product, viewModel.Image);
+                    // await _productService.CreateProductAsync(viewModel);
+                    return RedirectToAction("Index");
+
+                }
+
+                ModelState.AddModelError("", "Something Went Wrong.");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> ProductDetails(ProductRegVM viewModel)
+        {
+            var product = await _productService.GetProductAsync(viewModel.ArticleNumber);
+
+            if (product != null)
+            {
+                
+                viewModel.ProductName = product.ProductName;
+                viewModel.ProductDescription = product.ProductDescription;
+                
+
+                return View(viewModel);
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllAsync();
+            return View(products);
+        }
+    }
+}
